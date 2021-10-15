1d75f3ea1859375251f201942ae3bac599ef37a8
15davidallison <15david.allison@gmail.com>
2021-10-15 14:31:40 -0700

    Update install_jenkins.sh
    Some easily automatable processes added, still need pipeline creation / jenkins configuration to be automated

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-sudo yum update –y
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+# install and start jenkins
+sudo yum install java-1.8.0 -y 
+sudo yum remove java-1.7.0-openjdk
+sudo amazon-linux-extras install epel -y
+sudo yum install git -y
+sudo yum update -y 
+sudo yum install jenkins java-1.8.0-openjdk-devel
+sudo yum -y install wget
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum upgrade
-sudo yum install jenkins java-1.8.0-openjdk-devel -y
-sudo systemctl daemon-reload
+sudo yum upgrade -y
+sudo yum install jenkins -y
 sudo systemctl start jenkins
-sudo systemctl status jenkins
+
+# print out the admin password for jenkins
+sudo su
+echo "initialAdminPassword: "
+cat /var/lib/jenkins/secrets/initialAdminPassword
+
+# install terraform
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
